0429ac150701d4c9311ce29cd6ed54908a4f412e
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-03-16 16:25:36 -0700

    edit link

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -26,7 +26,7 @@ const PostItem = (props) => {
 
   return (
     <li className={classes.post}>
-      <Link href={postPath}>
+      <Link href="/posts/[slug]" as={`/posts/${slug}`}>
         <a>
           <div className={classes.image}>
             <Image
